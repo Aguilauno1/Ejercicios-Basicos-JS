3dbc8907a47c420b22092fb97e0eb5bc468c1ab4
Aguilauno <olivermag02@gmail.com>
2025-08-08 19:32:01 +0200

    ejercicio 3 completado

--- ejercicio3.js
+++ ejercicio3.js
@@ -1,6 +1,17 @@
-const jedi = { nombre: 'Luke Skywalker', edad: 19 }
+// 3.1 Multiplicacion
+console.log(10 * 5)
+// 3.2 Division
+console.log(10 / 2)
+// 3.3 Resto
+console.log(15 % 9)
+// 3.4 Operador de asignacion -> o = 15
+let p = 10,
+  j = 5
+let o = p + j
+console.log(o)
 
-// Tu código para cambiar la edad aquí...
-jedi.edad = 25
-
-console.log(jedi)
+// 3.5 Operador de asignacion -> i = 50
+let c = 10,
+  m = 5
+let i = c * m
+console.log(i)
