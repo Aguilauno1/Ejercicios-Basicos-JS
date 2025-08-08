d933015a4298a982703e1fee60f8910d73e111e9
Aguilauno <olivermag02@gmail.com>
2025-08-08 18:53:02 +0200

    ejercicio 1

---
+++ ejercicio1.js
@@ -0,0 +1,15 @@
+// 1. Variables básicas
+let miSuperheroeFavorito = 'Hulk'
+let numeroFavorito = 50
+let altura = 5
+let longitud = 10
+
+// 2. Suma
+let suma = altura + longitud
+
+// 3. Imprimir todas
+console.log(miSuperheroeFavorito)
+console.log(numeroFavorito)
+console.log(altura)
+console.log(longitud)
+console.log(suma)
